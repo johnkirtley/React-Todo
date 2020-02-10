57e8c4dc7c160d63b8c0f33016f59b240f7beccc
johnkirtley <kirtleyj16@gmail.com>
2020-02-10 13:53:32 -0800

    Todo items rendering on page

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Task = (props) => {
+    return (
+        <div>
+            {props.item.task}
+        </div>
+    )
+}
+
+export default Task;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,17 @@
+import React from 'react';
+import Todo from './Todo';
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+const TodoList = (props) => {
+    return (
+        <div>
+            {props.listItems.map(item => (
+                <Todo key={item.id} item={item} />
+            ))}
+        </div>
+    )
+}
+
+export default TodoList;
